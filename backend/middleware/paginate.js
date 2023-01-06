c511269b9f902eea47cb17f47bbb287f787872b1
Jason Garcia <jaygarcia0530@gmail.com>
2023-01-05 22:12:32 -0800

    sent total number of collections after filter in paginate middleware

--- backend/middleware/paginate.js
+++ backend/middleware/paginate.js
@@ -1,35 +1,38 @@
 
-const paginate =  (model) => {
+const paginate = (model) => {
     return async (req, res, next) => {
         const id = req.query.id
         const page = parseInt(req.query.page);
         const limit = parseInt(req.query.limit);
+        const theme = req.query.theme;
 
         const startIndex = (page - 1) + limit;
         const endIndex = page * limit;
 
         const results = {};
 
-        console.log("count documents", await model.countDocuments().exec())
+        console.log("count documents", await model.countDocuments({author: id, theme: theme }).exec())
+        const total = await model.countDocuments({author: id, theme: theme }).exec();
 
-        if(endIndex < await model.countDocuments().exec()) {
+        if (endIndex < total) {
             results.next = {
                 page: page + 1,
                 limit: limit
             }
         }
 
-        if(startIndex > 0) {
+        if (startIndex > 0) {
             results.previous = {
                 page: page - 1,
                 limit: limit
             }
         }
-        try{
-            results.results = await model.find({author: id}).limit(limit).skip(startIndex).sort({createdAt: -1}).populate('author').exec();
+        try {
+            results.results = await model.find({ author: id, theme: theme }).limit(limit).skip(startIndex).sort({ createdAt: -1 }).populate('author').exec();
+            results.total = total
             res.paginatedResults = results
-        }catch(e){
-            res.status(500).json({message: e.message})
+        } catch (e) {
+            res.status(500).json({ message: e.message })
         }
 
         res.paginatedResults = results;
